cfa087b0164e80880bb114420e71e4e153ecec15
maxiggle <clsyfriday@gmail.com>
2024-08-08 08:54:15 +0100

    text

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,9 @@ export default function Login(){
     return (
         <div className="login-container" >
             <img src={zephyIcon} alt="zephyIcon" className="w-[500px]"></img>
-            
+            <div className="">
+                <div className="text-3xl font-bold flex">Welcome to Fleet Finance</div>
+            </div>
         </div>
     )
 }
