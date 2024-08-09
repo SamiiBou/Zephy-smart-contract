b6d3583471b9521feb2605c065f6e4029edbc8d8
maxiggle <clsyfriday@gmail.com>
2024-08-09 02:51:56 +0100

    wip: default avatars for pfp

--- client/src/pages/SelectAvatar.jsx
+++ client/src/pages/SelectAvatar.jsx
@@ -1,11 +1,11 @@
 export default function SelectAvatar() {
-    const avatars = {
-        "1": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-        "2": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-        "3": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-        "4": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-        "5": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
-    }
+    // const avatars = {
+    //     "1": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    //     "2": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    //     "3": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    //     "4": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    //     "5": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    // }
     return <div className="flex items-center justify-center min-h-screen p-4">
         <div className="w-full max-w-6xl h-[600px] bg-white rounded-xl shadow-lg overflow-hidden flex flex-col md:flex-row"></div>
     </div>
