e666ca636dcd190dc5bf77cf5bfd7fdaa4684dd3
Treasure O <treasure@tronweb.co>
2024-08-10 12:04:05 +0100

    pay

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SelectAvatar from "./pages/SelectAvatar";
+import Invoice, { Loader as InvoiceLoader } from "./pages/Invoice";
 import Dashboard from "./components/layouts/Dashboard";
 
 import DashboardHome from "./pages/dashboard/Home";
@@ -24,6 +25,11 @@ const router = createBrowserRouter([
   {
     path: "select-avatar",
     element: <SelectAvatar />,
+  },
+  {
+    path: "pay/:ref",
+    element: <Invoice />,
+    loader: InvoiceLoader,
   },
   {
     path: "/dashboard",
